6bd6e165fbe9087d681b72453cd7b173a5c419f4
jayasree687 <137481237+jayasree687@users.noreply.github.com>
2023-11-07 16:58:50 +0530

    Add files via upload

---
+++ OrbitControls.js
@@ -0,0 +1,7 @@
+<script src="OrbitControls.js"></script>
+let controls = new THREE.OrbitControls(camera);
+controls.addEventListener('change', renderer);
+function animate() {
+	renderer.render(scene,camera);
+	requestAnimationFrame(animate);
+  }
